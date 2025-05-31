9ace4bc55da6be23f3cd48d06bf8608888940643
Will Dollinger <40767766+williamralphdollinger@users.noreply.github.com>
2025-05-31 01:57:31 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Crave Theory</h1>
+      <p>This is your clean Next.js site. No mess. No stress.</p>
+    </div>
+  );
+}
