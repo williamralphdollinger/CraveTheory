f758a6c576691a0f0fa0b9482e3672e43dfd270b
Will Dollinger <40767766+williamralphdollinger@users.noreply.github.com>
2025-05-31 05:08:50 -0400

    Create -app.js

---
+++ pages/-app.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Montserrat:wght@400;600;700&family=Roboto:wght@400&display=swap"
+          rel="stylesheet"
+        />
+        <style>{`
+          :root {
+            --font-logo: 'Montserrat', sans-serif;
+            --font-tagline: 'Lato', sans-serif;
+            --font-body: 'Roboto', sans-serif;
+          }
+        `}</style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
